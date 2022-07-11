68798bd738d55a01dcc89d72565e58579d85db4b
megler <majeresmom@att.net>
2022-07-11 09:27:44 -0400

    push to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pw';
+export class AppComponent {  
+  password = ''
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false
+  length = 0
+
+  onChangeLength(value: string){       
+    this.length = parseInt(value)    
+  }
+
+  onChangeUseLetters(){    
+    this.includeLetters = !this.includeLetters
+  }
+  onChangeUseNumbers(){    
+    this.includeNumbers = !this.includeNumbers
+  }
+  onChangeUseSymbols(){    
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onButtonClick() {
+    const numbers = '123467890'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()'
+
+    let validChars = ''
+
+    if (this.includeLetters) {
+      validChars += letters
+    }
+    if (this.includeNumbers) {
+      validChars += numbers
+    }
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+    
+    let generatedPassword = ''
+
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length)  
+      generatedPassword += validChars[index]
+      
+    }   
+
+    this.password = generatedPassword
+  }
+
 }
